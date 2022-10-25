fa03725ff4e60ac7e00cf46db272dc131ea6a3a8
BiniLemma <61507289+BiniLemma@users.noreply.github.com>
2022-10-25 23:21:03 +0300

    initial commit

---
+++ src/test/java/com/amigoscode/demo/EmailValidatorTest.java
@@ -0,0 +1,31 @@
+package com.amigoscode.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EmailValidatorTest {
+
+    private final EmailValidator underTest = new EmailValidator();
+
+    @Test
+    public void itShouldValidateCorrectEmail(){
+        assertThat(underTest.test("hello@gmail.com")).isTrue();
+
+    }
+
+    @Test
+    public void itShouldValidateAnInCorrectEmail(){
+        assertThat(underTest.test("hellogmail.com")).isFalse();
+
+    }
+
+    @Test
+    public void itShouldValidateAnInCorrectEmailWithoutDotAtTheEnd(){
+        assertThat(underTest.test("hello@gmail")).isFalse();
+
+    }
+
+
+
+}
